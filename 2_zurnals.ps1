74933383629b1cc4a41d06505a228c2f45a786a5
kortoshka075-cell <kortoshka075@gmail.com>
2026-03-19 13:37:24 +0200

    Update 2_zurnals.ps1

--- 2_zurnals.ps1
+++ 2_zurnals.ps1
@@ -1,1 +1,7 @@
+$logs = Get-WinEvent -LogName Application | Where-Object { 
+$_.LevelDisplayName -eq "Warning" -and $_.TimeCreated -gt (Get-Date).AddDays(-3)}
 
+$logs | Out-File "$env:USERPROFILE\Documents\Warning.txt"
+
+$logs | Group-Object ProviderName | Sort-Object Count -Descending | 
+Select-Object -First 3 | Out-File "$env:USERPROFILE\Documents\2_zurnals.ps1"
